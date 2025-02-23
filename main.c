d8d1315952da1a2562b3ff73757e0afc822a2e43
jsoulier <jaansoulier@gmail.com>
2025-02-23 12:42:01 -0500

    initial

---
+++ main.c
@@ -0,0 +1,12 @@
+#define SDL3_GPU_DBG_DRAW_IMPL
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <stdlib.h>
+#include "sdl3_gpu_dbg_draw.h"
+
+int main(
+    int argc,
+    char** argv)
+{
+    return EXIT_SUCCESS;
+}
